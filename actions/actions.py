a4d6c8d74a8e479b1aa8280ad05b9e9832dea75b
Đinh Văn Nam <itchange.nw@gmail.com>
2021-05-28 07:44:52 +0700

    update v1

--- actions/actions.py
+++ actions/actions.py
@@ -50,3 +50,20 @@ class ActionRecommend(Action):
             text="Em nghĩ hôm nay anh chị có thể thử món '{}' hoặc bên cạnh đó cũng có thể là món '{}' ạ".format(food[0], food[1]))
 
         return []
+        
+class action_get_lottery(Action):
+	def name(self):
+            # Doan nay khai bao giong het ten ham ben tren la okie
+		    return 'action_get_lottery'
+	def run(self, dispatcher, tracker, domain):
+            # Khai bao dia chi luu tru ket qua so xo. O day lam vi du nen minh lay ket qua SX Mien Bac
+            url = 'https://xskt.com.vn/rss-feed/mien-bac-xsmb.rss'
+            # Tien hanh lay thong tin tu URL
+            feed_cnt = feedparser.parse(url)
+            # Lay ket qua so xo moi nhat
+            first_node = feed_cnt['entries']
+            # Lay thong tin ve ngay va chi tiet cac giai
+            return_msg = first_node[0]['title'] + "\n" + first_node[0]['description']
+            # Tra ve cho nguoi dung
+            dispatcher.utter_message(return_msg)
+            return []
